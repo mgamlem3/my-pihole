e0090ff6049c76ebc9b0138f6352fd0a86559207
Michael Gamlem III <mgamlem3@gmail.com>
2022-06-30 22:23:42 -0700

    Remove —no-cache

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ "$1" == "" ]; then
 else
 	echo $1 >secret.txt
 fi
-docker build --no-cache -t pihole.mgamlem3.base .
+docker build -t pihole.mgamlem3.base .
 if [ "$2" != "replica" ]; then
 	docker build -t pihole.mgamlem3.primary -f Dockerfile.primary .
 fi
