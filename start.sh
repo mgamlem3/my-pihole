1a53f46b33ce4f90205f6ddbb366f3bf2c2d1f15
Michael Gamlem III <mgamlem3@gmail.com>
2022-07-09 22:02:14 -0700

    Specify volumes in start file

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@ if [ "$1" == "" ]; then
 	echo "You must specify which variant to start: primary or replica"
 	exit 1
 elif [ "$1" == "primary" ]; then
-	docker run -d -p 53:53 -p 80:80 --restart=unless-stopped --name pihole.mgamlem3.primary pihole.mgamlem3.primary
+	docker run -d -p 53:53 -p 80:80 -v pihole:/etc/pihole -v dnsmasq:/etc/dnsmasq.d --restart=unless-stopped --name pihole.mgamlem3.primary pihole.mgamlem3.primary
 	exit 0
 elif [ "$1" == "replica" ]; then
-	docker run -d -p 53:53 -p 80:80 --restart=unless-stopped --name pihole.mgamlem3.replica pihole.mgamlem3.replica
+	docker run -d -p 53:53 -p 80:80 -v pihole:/etc/pihole -v dnsmasq:/etc/dnsmasq.d --restart=unless-stopped --name pihole.mgamlem3.replica pihole.mgamlem3.replica
 	exit 0
 else
 	exit 1
